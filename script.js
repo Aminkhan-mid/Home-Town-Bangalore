f3f1adbf2adde0bfb350d5c4142386a8cbdaac71
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2024-10-18 16:24:10 +0530

    add the files from the folder.

---
+++ script.js
@@ -0,0 +1,100 @@
+const lalRatings = document.getElementById("lalRating")
+const lalMore = document.getElementById("lalKnow")
+
+const cubbonRatings = document.getElementById("cubbonRating")
+const cubbonMore = document.getElementById("cubbonKnow")
+
+const palaceRatings = document.getElementById("palaceRating")
+const palaceMore = document.getElementById("palaceKnow")
+
+const electTodo = document.getElementById("electornic")
+
+const itpRating = document.getElementById("itpRatings")
+
+const iiscMore = document.getElementById("IIScMore")
+
+const ubcityThings = document.getElementById("ubcityThings")
+const ubcityRatings = document.getElementById("ubcityRating")
+
+const phoenixRatings = document.getElementById("phoenixRating")
+const phoenixThings = document.getElementById("phoenixThings")
+
+const commercialThings = document.getElementById("commercialThings")
+const commercialRatings = document.getElementById("commercialRatings")
+
+lalRatings.addEventListener("click", function(){
+  const url = "https://www.google.com/search?sa=X&sca_esv=e3415725a8787c2e&rlz=1C5CHFA_enIN1126IN1126&q=Lalbagh+Botanical+Garden&si=ACC90nw6pbWgl1C1yAf0ViotehKE79dypRcSLSQypDe0LTIzow4kMRxVKK1OXJlzG_0oRARmBYWUgPtUgYCdWlHFXSacklpDPTY_2o5Lt8pq_TMpNYMQR8ORYGDjJ7F3dLNnOd0Khct006HutBqCAW4wvakq_AYATg%3D%3D&ved=2ahUKEwiC0O_wupWJAxVzyzgGHfigDgAQmJ0LegQIMBAA&biw=1163&bih=616&dpr=2.2"
+
+  window.open(url, "_blank")
+})
+
+lalMore.addEventListener("click", function(){
+    const url = "https://en.wikipedia.org/wiki/Lal_Bagh"
+    window.open(url, "_blank")
+})
+
+cubbonRatings.addEventListener("click", function(){
+  const url = "https://www.google.com/search?sca_esv=9665476702a22ff3&rlz=1C5CHFA_enIN1126IN1126&q=Shri+Ch%C4%81marajendra+(Cubbon)+Park&si=ACC90nw6pbWgl1C1yAf0ViotehKE79dypRcSLSQypDe0LTIzo7vnR7F-fzDmmA45NQb1SgJ0hftoSBGnz0QsBHuG1egXBSw4QcgMXV6htRlRUEERFN-EcqIPrcC21govs5bz7vFD6zJD3yWn3V6Dky5y3803LabspA%3D%3D&sa=X&ved=2ahUKEwiexOm37ZaJAxVgTmwGHa0xDnsQmJ0LegQIJBAA&biw=1163&bih=616&dpr=2.2"
+  window.open(url, "_blank")
+})
+cubbonMore.addEventListener("click", function(){
+  const url = "https://en.wikipedia.org/wiki/Sri_Chamarajendra_Udyanavana"
+  window.open(url, "_blank")
+})
+
+palaceRatings.addEventListener("click", function(){
+  const url = "https://www.google.com/search?rlz=1C5CHFA_enIN1126IN1126&si=ACC90nyCYBbWqtQm7cEukhYWJa96Z83HPUdXL2D5-1SvBDfWrK_k7ONPiAngbN7zyXgkmVxg4VGGWYpbBuSCZcohmzv9n9JWlQkSjKZIVEAWzIuWcYJ_w-kqGQ0nkiQUgPtxxDLb8cG5rsTpo5Ntc6qqCLrpnlx0Tw%3D%3D&q=Bengaluru%20Palace%20Reviews&sa=X&ved=0CBUQqe0LahcKEwjYrrSq7ZaJAxUAAAAAHQAAAAAQNQ&biw=1163&bih=616&dpr=2.2"
+  window.open(url, "_blank")
+})
+
+palaceMore.addEventListener("click", function(){
+  const url = "https://en.wikipedia.org/wiki/Bangalore_Palace"
+  window.open(url, "_blank")
+})
+
+electTodo.addEventListener("click", function(){
+  const url = "https://mygate.com/blog/neighbourhood/electronic-city/"
+  window.open(url, "_blank")
+})
+
+
+ubcityRatings.addEventListener("click", function(){
+  const url = "https://www.google.com/search?rlz=1C5CHFA_enIN1126IN1126&si=ACC90nyCYBbWqtQm7cEukhYWJa96Z83HPUdXL2D5-1SvBDfWrK_k7ONPiAngbN7zyXgkmVxR7q4O4q3lgtOpqkAtZsdblD0PMKTJa-1cSoL2uYabanBSxJLjdbIyfDEjte8X_wYDIFLM&q=UB%20City%20Reviews&sa=X&ved=0CBUQqe0LahcKEwjIoNqompeJAxUAAAAAHQAAAAAQBw&biw=1163&bih=616&dpr=2.2"
+  window.open(url, "_blank")
+})
+
+ubcityThings.addEventListener("click", function(){
+  const url = "https://www.seawatersports.com/places/karnataka/ub-city"
+  window.open(url, "_blank")
+})
+
+
+itpRating.addEventListener("click", function(){
+  const url = "https://www.google.com/search?q=international+tech+park+bangalore&rlz=1C5CHFA_enIN1126IN1126&sca_esv=339a018dea4bc811&cs=0&biw=1163&bih=616&ei=mwUSZ728N-2MseMPmrbz0QU&gs_ssp=eJwFwcsNgCAMANB41Cnw4NkiBMUR3KK19YtgCAfH972m7p8eKNiLoZo7-AyhaL2RtQCaHc_wOQAx4p2fmAbW49KesUiOWM4UMagi66FezLcijDuGlOUHKtQa7A&oq=internation+tech+park+&gs_lp=Egxnd3Mtd2l6LXNlcnAiFmludGVybmF0aW9uIHRlY2ggcGFyayAqAggKMg0QLhiABBjHARgKGK8BMgcQABiABBgKMgcQABiABBgKMgcQABiABBgKMgcQABiABBgKMg0QLhiABBjHARgKGK8BMgcQABiABBgKMgcQABiABBgKMgcQABiABBgKMg0QLhiABBjHARgKGK8BMhwQLhiABBjHARgKGK8BGJcFGNwEGN4EGOAE2AECSNeLAVC0NViVenAEeAGQAQCYAboBoAHFGKoBBDEuMjS4AQHIAQD4AQGYAh2gAv0aqAIKwgITEAAYgAQYQxi0AhiKBRjqAtgBAcICHBAuGIAEGEMYtAIYxwEYigUY6gIYjgUYrwHYAQHCAhkQLhiABBhDGLQCGMcBGIoFGOoCGK8B2AEBwgIQEC4YgAQYQxjHARiKBRivAcICChAAGIAEGEMYigXCAg4QABiABBixAxiDARiKBcICCxAAGIAEGLEDGIMBwgIWEC4YgAQYsQMY0QMYQxiDARjHARiKBcICHxAuGIAEGEMYxwEYigUYrwEYlwUY3AQY3gQY4ATYAQLCAhoQLhiABBiRAhjHARiYBRiZBRiKBRieBRivAcICCxAAGIAEGJECGIoFwgIIEC4YgAQYsQPCAggQABiABBixA8ICDhAuGIAEGLEDGNEDGMcBwgIpEC4YgAQYkQIYxwEYmAUYmQUYigUYngUYrwEYlwUY3AQY3gQY4ATYAQLCAgsQLhiABBiRAhiKBcICEBAuGIAEGLEDGEMYgwEYigXCAhMQLhiABBhDGMcBGIoFGI4FGK8BwgIQEAAYgAQYsQMYQxiDARiKBcICDRAuGIAEGEMY1AIYigXCAhoQLhiABBiRAhiKBRiXBRjcBBjeBBjgBNgBAsICERAuGIAEGJECGMcBGIoFGK8BwgINEC4YgAQYsQMYQxiKBcICChAuGIAEGEMYigXCAhEQLhiABBixAxjRAxiDARjHAcICIBAuGIAEGJECGMcBGIoFGK8BGJcFGNwEGN4EGOAE2AECwgIXEC4YgAQYkQIYsQMYxwEYigUYjgUYrwHCAiYQLhiABBiRAhixAxjHARiKBRiOBRivARiXBRjcBBjeBBjgBNgBAsICDRAAGIAEGLEDGEMYigXCAgUQABiABMICBBAAGAPCAgYQABgDGArCAgoQABiABBixAxgKwgINEC4YgAQY0QMYxwEYCsICBxAuGIAEGArCAhAQLhiABBjHARgKGI4FGK8BwgITEC4YgAQYsQMY0QMYgwEYxwEYCpgDBboGBAgBGAe6BgYIAhABGBSSBwY0LjI0LjGgB7PwAg&sclient=gws-wiz-serp"
+  window.open(url, "_blank")
+})
+
+
+iiscMore.addEventListener("click", function(){
+  const url = "https://en.wikipedia.org/wiki/Indian_Institute_of_Science"
+  window.open(url, "_blank")
+})
+
+phoenixRatings.addEventListener("click", function(){
+  const url = "https://www.google.com/search?rlz=1C5CHFA_enIN1126IN1126&si=ACC90nyCYBbWqtQm7cEukhYWJa96Z83HPUdXL2D5-1SvBDfWrK_k7ONPiAngbN7zyXgkmVxmOqZVT7X4DGLfnVTSkcEWxlWNQZDuzcvAjnnsud9k0-Na7bdi37pZ3gmXItGjibFPIpbJXdINFaYxE6mx0fU2UNx-bw%3D%3D&q=Phoenix%20Marketcity%20Reviews&sa=X&ved=0CBQQqe0LahcKEwiIn62cqpeJAxUAAAAAHQAAAAAQBw&biw=1163&bih=616&dpr=2.2"
+  window.open(url, "_blank")
+})
+
+phoenixThings.addEventListener("click", function(){
+  const url = "https://www.phoenixmarketcity.com/bangalore/tourist"
+  window.open(url, "_blank")
+})
+
+commercialThings.addEventListener("click", function(){
+  const url = "https://staybloom.com/chapters/commercial-street-bangalore"
+  window.open(url, "_blank")
+})
+commercialRatings.addEventListener("click", function() {
+  const url = "https://www.google.com/search?q=things+to+do+in+bangalore&sca_esv=339a018dea4bc811&rlz=1C5CHFA_enIN1126IN1126&udm=15&sa=X&ved=2ahUKEwjw3eOerZeJAxUnXGwGHXUlBB8QxN8JegQIVRAz&biw=1163&bih=616&dpr=2.2#vhid=/g/11bxfz11gp&vssid=ttduv-_1";
+  window.open(url, "_blank");
+});
